9c86f576eba510c32d8b678569d042e31445b35f
Connor Clark <cjamcl@gmail.com>
2022-03-09 18:56:07 -0800

    emscripten build in working condition now

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -6,20 +6,22 @@ mkdir -p build_emscripten
 cd build_emscripten
 
 USE_FLAGS=(
-  -s USE_FREETYPE=1
-  -s USE_VORBIS=1
-  -s USE_OGG=1
-  -s USE_LIBJPEG=1
-  -s USE_SDL=2
-  -s USE_LIBPNG=1
-  -s FULL_ES2=1
-  -s ASYNCIFY
-  -s TOTAL_MEMORY=2147418112
-  -O3
-)
+    -s USE_FREETYPE=1
+    -s USE_VORBIS=1
+    -s USE_OGG=1
+    -s USE_LIBJPEG=1
+    -s USE_SDL=2
+    -s USE_LIBPNG=1
+    -s FULL_ES2=1
+    -s ASYNCIFY
+    -s TOTAL_MEMORY=2147418112
+    -O3
+  )
 
 # might need to set this
-# EM_CACHE=/Users/connorclark/tools/emsdk/upstream/emscripten/cache
+EM_CACHE=/Users/connorclark/tools/emsdk/upstream/emscripten/cache
+
+# exit 0
 
 emcmake cmake .. \
   -D CMAKE_BUILD_TYPE=Release \
@@ -31,11 +33,11 @@ emcmake cmake .. \
   -D WANT_TESTS=OFF \
   -D WANT_OPENAL=OFF \
   -D ALLEGRO_WAIT_EVENT_SLEEP=ON \
-  -D SDL2_INCLUDE_DIR=$EM_CACHE/sysroot/include \
-  -D CMAKE_C_FLAGS="${USE_FLAGS}" \
-  -D CMAKE_CXX_FLAGS="${USE_FLAGS}" \
-  -D CMAKE_EXE_LINKER_FLAGS="${USE_FLAGS}" \
+  -D SDL2_INCLUDE_DIR="$EM_CACHE/sysroot/include" \
+  -D CMAKE_C_FLAGS="${USE_FLAGS[*]}" \
+  -D CMAKE_CXX_FLAGS="${USE_FLAGS[*]}" \
+  -D CMAKE_EXE_LINKER_FLAGS="${USE_FLAGS[*]}" \
   -D CMAKE_EXECUTABLE_SUFFIX_CXX=".html"
 
 # WIP: errors due to https://github.com/emscripten-core/emscripten/issues/15325
-cmake --build . -t al_example
+emmake make al_example
